bc6cfa351a061c7ff1928416c5d2c943bc4dbf71
Crypto rrivem <crypto.rrivem@gmail.com>
2022-06-22 16:51:04 -0300

    configurable jimp

--- README.md
+++ README.md
@@ -1,19 +1,34 @@
-# Tinify tool
+# Optimization tool
+
+## Usage - tinify.com API
 
 Allows to use tinify.com api to optimize and resize images. First, you'll need to update `config.json` with your [tinify.com api key](https://tinify.com/dashboard/api).
 
-## Usage
+To run the optimization:
 
 ```
 node tinify.js config.json input output
 ```
 
+- `config.json` is the configuration file (more info below)
 - `input` is where you have your source files to optimize
 - `output` is where your optimized files will go, with the same name they were on `input`
 
+## Usage - jimp
+
+Another tool for optimizing images, runs locally
+
+```
+node jimp.js config.json input output
+```
+
 ## config.json
 
+- `"key": "API-KEY"` api key for using tinify.com script
 - `"overwrite": false` will skip files that exists in `output` folder, set to `true` to reprocess the source and overwrite the output
-- `"fileExtension": ".png"` extension of the files you want to optimize
+- `"concurrence": 10` parallel optimizations, the higher, the more memory it requires
+- `"inputExtension": ".png"` extension of the files you want to optimize
+- `"outputExtension": ".jpg"` extension of the optimized files
 - `"resize": {...}` configuration for the [resize options](https://tinypng.com/developers/reference/nodejs#resizing-images)
+- `"quality": 90` compression quality
 - `"samples": 3` only process this amount of files, useful for preview
--- jimp.js
+++ jimp.js
@@ -1,18 +1,44 @@
 const Jimp = require("jimp");
 const fs = require("fs");
 const path = require("path");
-const [_1, _2, inputFolder, outputFolder] = process.argv;
-const files = fs.readdirSync(inputFolder);
+const cliProgress = require("cli-progress");
+const { PromisePool } = require("@supercharge/promise-pool");
+const [_1, _2, configFile, inputFolder, outputFolder] = process.argv;
+const config = JSON.parse(fs.readFileSync(configFile, "utf-8"));
 
-files.forEach((f) =>
-  Jimp.read(path.join(inputFolder, f), (err, file) => {
-    if (err) throw err;
-    const output = path.join(
-      outputFolder,
-      `${path.basename(f, path.extname(f))}.jpg`
-    );
-    file
-      .quality(90) // set JPEG quality
-      .write(output); // save
-  })
-);
+let files = fs
+  .readdirSync(inputFolder)
+  .filter(
+    (f) =>
+      f.endsWith(config.inputExtension) &&
+      (config.overwrite ||
+        !fs.existsSync(
+          path.join(
+            outputFolder,
+            `${path.basename(f, path.extname(f))}${config.outputExtension}`
+          )
+        ))
+  );
+if (config.samples) {
+  files = files.slice(0, config.samples);
+}
+const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+
+(async function () {
+  bar.start(files.length, 0);
+  await PromisePool.withConcurrency(config.concurrence)
+    .for(files)
+    .handleError(console.error)
+    .process(async (f) => {
+      const file = await Jimp.read(path.join(inputFolder, f));
+      const output = path.join(
+        outputFolder,
+        `${path.basename(f, path.extname(f))}${config.outputExtension}`
+      );
+      await file
+        .quality(config.quality) // set JPEG quality
+        .writeAsync(output); // save
+      bar.increment();
+    });
+  bar.stop();
+})();
